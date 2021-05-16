c7f9dc4b71efe3d022123137027faf341eadfb75
dianamiscuta <diana.miscuta@student.upt.ro>
2021-05-16 18:19:33 +0300

    added style to add event screen

--- src/main/java/org/app/events/event/controllers/AddEventController.java
+++ src/main/java/org/app/events/event/controllers/AddEventController.java
@@ -1,13 +1,19 @@
 package org.app.events.event.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.app.events.event.exceptions.EventAlreadyExistsException;
 import org.app.events.event.services.EventService;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -18,7 +24,7 @@ public class AddEventController
     @FXML
     private DatePicker date;
     @FXML
-    private TextField description;
+    private TextArea description;
     @FXML
     private Button backBtn;
     @FXML
@@ -44,4 +50,12 @@ public class AddEventController
         }
     }
 
+    public void changeToMainPage() throws IOException
+    {
+        Stage stage = (Stage)backBtn.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("./mainPage/MainPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
